acbd4600a86a18f68fca4b678ce627d54bb1a166
Michael Griehm <griehmmj@gmail.com>
2022-03-10 16:35:32 -0500

    Added Data Lake iac and cicd

---
+++ iac/data-lake.tf
@@ -0,0 +1,97 @@
+data "azuread_user" "admin" {
+  user_principal_name = var.admin_user_principal_name
+}
+
+resource "azuread_group" "stream_admin_group" {
+  display_name     = "${azurerm_storage_account.adls.name}-stream-admin"
+  security_enabled = true
+
+  owners = [
+    data.azurerm_client_config.current.object_id,
+    data.azuread_user.admin.object_id
+  ]
+}
+
+resource "azuread_group_member" "admin_group_member" {
+  group_object_id  = azuread_group.stream_admin_group.object_id
+  member_object_id = data.azuread_user.admin.object_id
+}
+
+resource "azuread_group" "stream_writer_group" {
+  display_name     = "${azurerm_storage_account.adls.name}-stream-writer"
+  security_enabled = true
+
+  owners = [
+    data.azurerm_client_config.current.object_id,
+    data.azuread_user.admin.object_id
+  ]
+}
+
+resource "azuread_group" "stream_reader_group" {
+  display_name     = "${azurerm_storage_account.adls.name}-stream-reader"
+  security_enabled = true
+
+  owners = [
+    data.azurerm_client_config.current.object_id,
+    data.azuread_user.admin.object_id
+  ]
+}
+
+resource "azurerm_storage_account" "adls" {
+  name                     = local.a_name
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = "true"
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "crypto_quotes_filesystem" {
+  name               = "crypto-quotes"
+  storage_account_id = azurerm_storage_account.adls.id
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "stream_path" {
+  storage_account_id = azurerm_storage_account.adls.id
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.crypto_quotes_filesystem.name
+  resource           = "directory"
+  path               = "stream"
+
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.stream_admin_group.object_id
+    permissions = "rwx"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.stream_admin_group.object_id
+    permissions = "rwx"
+  }
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.stream_reader_group.object_id
+    permissions = "r--"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.stream_reader_group.object_id
+    permissions = "r--"
+  }
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.stream_writer_group.object_id
+    permissions = "-w-"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.stream_writer_group.object_id
+    permissions = "-w-"
+  }
+}
---
+++ iac/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azurerm = "~> 2.33"
+    azuread = "~>2.0"
+  }
+
+  # backend "azurerm" {
+  # }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "app_name" {
+  default   = "adls"
+  type      = string
+  sensitive = false
+}
+
+variable "env" {
+  default   = "demo"
+  sensitive = false
+}
+
+variable "location" {
+  default   = "East US 2"
+  sensitive = false
+  type      = string
+}
+
+variable "tags" {
+  type = map(string)
+
+  default = {
+    environment = "demo"
+    workload    = "crypto-analytics"
+  }
+}
+
+variable "admin_user_principal_name" {
+  type        = string
+  sensitive   = true
+  description = "The user principal name of the admin for the app."
+}
+
+locals {
+  loc            = lower(replace(var.location, " ", ""))
+  a_name         = replace(var.app_name, "-", "")
+  fqrn           = "${var.app_name}-${var.env}-${local.loc}"
+  fqrn_condensed = "${length(local.a_name) > 22 ? substr(local.a_name, 0, 22) : local.a_name}${substr(local.loc, 0, 1)}${substr(var.env, 0, 1)}"
+  rg_name        = local.fqrn
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = local.rg_name
+}
