4b0361d86b7a21d6d26b6170d5b94ca6c8ebbd41
Michael Griehm <griehmmj@gmail.com>
2022-03-11 10:08:11 -0500

    Changed storage account name to avoid name collision.

--- iac/data-lake.tf
+++ iac/data-lake.tf
@@ -38,7 +38,7 @@ resource "azuread_group" "stream_reader_group" {
 }
 
 resource "azurerm_storage_account" "adls" {
-  name                     = local.a_name
+  name                     = "${local.a_name}cryptoanalytics"
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
