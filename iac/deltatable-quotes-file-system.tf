d9ce7a8b49fee8188d67bebb151f549a4c204e67
Michael Griehm <griehmmj@gmail.com>
2022-04-22 10:29:48 -0400

    created unmanaged and managed quotes folders in delta lake

--- iac/deltatable-quotes-file-system.tf
+++ iac/deltatable-quotes-file-system.tf
@@ -40,11 +40,55 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "crypto_deltatable_filesyst
   }
 }
 
-resource "azurerm_storage_data_lake_gen2_path" "crypto_deltatable_quotes_by_day_spark_partition_path" {
+resource "azurerm_storage_data_lake_gen2_path" "crypto_deltatable_unmanaged_quotes" {
   storage_account_id = azurerm_storage_account.adls.id
   filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.crypto_deltatable_filesystem.name
   resource           = "directory"
-  path               = "quotes"
+  path               = "unmanaged/quotes"
+
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_admin_group.object_id
+    permissions = "rwx"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_admin_group.object_id
+    permissions = "rwx"
+  }
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_reader_group.object_id
+    permissions = "r--"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_reader_group.object_id
+    permissions = "r--"
+  }
+  ace {
+    scope       = "default"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_writer_group.object_id
+    permissions = "-w-"
+  }
+  ace {
+    scope       = "access"
+    type        = "group"
+    id          = azuread_group.deltatable_quotes_writer_group.object_id
+    permissions = "-w-"
+  }
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "crypto_deltatable_managed_quotes" {
+  storage_account_id = azurerm_storage_account.adls.id
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.crypto_deltatable_filesystem.name
+  resource           = "directory"
+  path               = "managed/quotes"
 
   ace {
     scope       = "default"
